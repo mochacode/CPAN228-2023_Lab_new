89939079854a989e59a5cb171c8198a7262c4572
Kimberly <kimberly.d102102@gmail.com>
2023-02-04 21:57:11 -0500

    lab 2

---
+++ src/main/java/com/cpan228/tekkenreborn/model/Fighter.java
@@ -0,0 +1,43 @@
+package com.cpan228.tekkenreborn.model;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Fighter {
+    @NotBlank
+    private String name;
+    @Max(100)
+    private int damagePerHit;
+    @Min(1000)
+    private int health;
+    @DecimalMin(value = "0.1", inclusive = true)
+    @DecimalMax(value = "10.0", inclusive = true)
+    private BigDecimal resistance;
+    private Anime animeFrom;
+
+    public enum Anime {
+        NARUTO("Naruto"), BLEACH("Bleach"), ONE_PIECE("One Piece"), TEKKEN("Tekken");
+
+        private String title;
+
+        private Anime(String title) {
+            this.title = title;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+}
+
+
