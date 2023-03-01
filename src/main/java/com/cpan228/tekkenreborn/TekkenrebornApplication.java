07a09a34ebc64d905991b3b64c45d5f14b2a7fd7
Kimberly <kimberly.d102102@gmail.com>
2023-02-28 20:57:09 -0500

    Lab 5

--- src/main/java/com/cpan228/tekkenreborn/TekkenrebornApplication.java
+++ src/main/java/com/cpan228/tekkenreborn/TekkenrebornApplication.java
@@ -5,16 +5,14 @@ import java.math.BigDecimal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import com.cpan228.tekkenreborn.controller.HomeController;
 import com.cpan228.tekkenreborn.model.Fighter;
 import com.cpan228.tekkenreborn.model.Fighter.Anime;
 import com.cpan228.tekkenreborn.repository.FighterRepository;
 
 /**
- * mvn spring-boot:run steps
+ * mvn spring-boot:run does following steps
  * 1. mvn clean
  * 2. mvn compile
  * 3. mvn package
@@ -24,6 +22,13 @@ import com.cpan228.tekkenreborn.repository.FighterRepository;
 @SpringBootApplication
 public class TekkenrebornApplication {
 
+	/**
+	 * This is the main method that starts the application
+	 * Spring Application Context is created here
+	 * You can configure your application properties using @param args
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TekkenrebornApplication.class, args);
 	}
@@ -32,32 +37,140 @@ public class TekkenrebornApplication {
 	public CommandLineRunner dataLoader(FighterRepository repository) {
 		return args -> {
 			repository.save(Fighter.builder()
-			.name("Heihachi Mishima")
-			.animeFrom(Anime.TEKKEN)
-			.damagePerHit(89)
-			.health(2000)
-			.resistance(new BigDecimal(0.5)).build());
+					.name("Heihachi Mishima")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(89)
+					.health(2000)
+					.resistance(new BigDecimal(0.5)).build());
 
 			repository.save(Fighter.builder()
-			.name("Kazuya Mishima")
-			.animeFrom(Anime.TEKKEN)
-			.damagePerHit(86)
-			.health(2100)
-			.resistance(new BigDecimal(0.5)).build());
+					.name("Kazuya Mishima")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(86)
+					.health(2100)
+					.resistance(new BigDecimal(0.5)).build());
 
 			repository.save(Fighter.builder()
-			.name("Jin Kazama")
-			.animeFrom(Anime.TEKKEN)
-			.damagePerHit(90)
-			.health(2200)
-			.resistance(new BigDecimal(0.5)).build());
+					.name("Jin Kazama")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(90)
+					.health(2200)
+					.resistance(new BigDecimal(0.5)).build());
 
 			repository.save(Fighter.builder()
-			.name("Steve Fox")
-			.animeFrom(Anime.TEKKEN)
-			.damagePerHit(80)
-			.health(25000)
-			.resistance(new BigDecimal(0.5)).build());
+					.name("Steve Fox")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(80)
+					.health(25000)
+					.resistance(new BigDecimal(0.5)).build());
+			repository.save(Fighter.builder()
+					.name("Hwoarang")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(89)
+					.health(2000)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Panda")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(86)
+					.health(2100)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Eddy Gordo")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(90)
+					.health(2200)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Steve Fox")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(80)
+					.health(25000)
+					.resistance(new BigDecimal(0.5)).build());
+			repository.save(Fighter.builder()
+					.name("Law")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(89)
+					.health(2000)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Lei Wulong")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(86)
+					.health(2100)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Anna")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(90)
+					.health(2200)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Xiaoyu")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(80)
+					.health(25000)
+					.resistance(new BigDecimal(0.5)).build());
+			repository.save(Fighter.builder()
+					.name("Brian Fury")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(89)
+					.health(2000)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Kuma")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(86)
+					.health(2100)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Paul Phoenix")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(90)
+					.health(2200)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("King")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(80)
+					.health(25000)
+					.resistance(new BigDecimal(0.5)).build());
+			repository.save(Fighter.builder()
+					.name("Bruce Irvin")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(89)
+					.health(2000)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Dragunov")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(86)
+					.health(2100)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Lars Alexandersson")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(90)
+					.health(2200)
+					.resistance(new BigDecimal(0.5)).build());
+
+			repository.save(Fighter.builder()
+					.name("Devil Jin")
+					.animeFrom(Anime.TEKKEN)
+					.damagePerHit(80)
+					.health(25000)
+					.resistance(new BigDecimal(0.5)).build());
 		};
 	}
 
---
+++ src/main/java/com/cpan228/tekkenreborn/repository/FighterRepositoryPaginated.java
@@ -0,0 +1,11 @@
+package com.cpan228.tekkenreborn.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.cpan228.tekkenreborn.model.Fighter;
+
+//It will be an interface that defines operations with the fighter
+//table in the database
+//This interface will extend PagingAndSortingRepository, which will allow us to retrieve fighters in pages
+public interface FighterRepositoryPaginated extends PagingAndSortingRepository<Fighter, Long> {
+}
