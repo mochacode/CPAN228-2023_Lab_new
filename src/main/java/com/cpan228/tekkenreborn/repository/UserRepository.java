f63848845ba6fa2a9e202dccae57ea8ff3bc6717
Kimberly <kimberly.d102102@gmail.com>
2023-03-16 10:40:58 -0400

    Lab 6

---
+++ src/main/java/com/cpan228/tekkenreborn/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cpan228.tekkenreborn.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cpan228.tekkenreborn.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
